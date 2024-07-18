f96454af931968f5f45c0ab6d3514ec3a7d52cf2
Arthur Ott <arthur.ott91@web.de>
2024-07-18 17:17:27 +0200

    hangman: add some test script

--- hangman/script.js
+++ hangman/script.js
@@ -25,3 +25,76 @@ a.addEventListener("click", () => {
 
   console.log(letterClicked);
 });
+
+// check if letter in words
+
+/*
+
+
+let words = ["javascript"];
+
+let split = words[0].split("");
+
+let result = [];
+
+ 
+
+ 
+
+function check(lett) {
+
+  // Wenn das result-Array noch leer ist, initialisieren wir es
+
+  if (result.length === 0) {
+
+    for (let i = 0; i < split.length; i++) {
+
+      if (!split[i].includes(lett)) {
+
+        result.push("_");
+
+      } else {
+
+        result.push(split[i]);
+
+      }
+
+    }
+
+  } else {
+
+    // Wenn das result-Array bereits initialisiert ist, aktualisieren wir es
+
+    for (let z = 0; z < split.length; z++) {
+
+      if (split[z] === lett && result[z] === "_") {
+
+        result[z] = lett;
+
+      }
+
+    }
+
+  }
+
+}
+
+ 
+
+// Beispielaufrufe der Funktion check
+
+check("a");
+
+ 
+
+console.log(result);
+
+check("j");
+
+console.log(result)
+
+check("v")
+
+console.log(result)
+
+*/
